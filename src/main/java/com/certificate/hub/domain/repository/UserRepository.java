ac30acaa081bae5eb79d3efe681df667f9baf4e9
KevinRinconN <estebanrincon0926@gmail.com>
2024-04-02 19:21:50 -0500

    Add findByEmail method to UserRepository

--- src/main/java/com/certificate/hub/domain/repository/UserRepository.java
+++ src/main/java/com/certificate/hub/domain/repository/UserRepository.java
@@ -3,5 +3,8 @@ package com.certificate.hub.domain.repository;
 import com.certificate.hub.persistence.entity.UserEntity;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends CrudRepository<UserEntity, String> {
+    Optional<UserEntity> findByEmail(String email);
 }
