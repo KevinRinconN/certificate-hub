2aa603efe70de42f7b39da0246c3e6d66ca65e71
KevinRinconN <estebanrincon0926@gmail.com>
2024-04-02 21:50:09 -0500

    add estandar custom response

---
+++ src/main/java/com/certificate/hub/domain/dto/response/ApiResponse.java
@@ -0,0 +1,17 @@
+package com.certificate.hub.domain.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ApiResponse<T> {
+    private String message;
+    private Status status;
+    private T data;
+}
+
